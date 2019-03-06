cc8f1871c039ff898cb47aa8aa8673d329b0b76b
MagicalBrain <13542593649@163.com>
2019-03-06 23:27:07 +0800

    更新一下

---
+++ AVL-Tree/AvlTree.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+#define TRUE 1
+#define FALSE 0
+
+#define OK 1
+#define ERROR 0
+//#define OVERFLOW -1
+
+typedef int Status;
+
+#define KeyType int
+
+struct AvlNode
+{
+	KeyType data;
+	AvlNode *left, *right;
+	int height;
+};
+
+
+struct AvlTree
+{
+	AvlNode *root;
+};
+
+//初始化函数
+void InitAvlTree(AvlTree &T)
+{
+	T.root = NULL;
+}
+
+//查找元素函数
+bool find(KeyType x, AvlTree T)
+{
+	AvlNode *t = T.root;
+
+	while (t != NULL && t->data != x)
+	{
+		if (t->data > x)
+			t = t->left;
+		else
+			t = t->right;
+	}
+	if (t == NULL)
+		return FALSE;
+	else
+		return TRUE;
+}
