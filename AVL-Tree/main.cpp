3d654c4547305e7a5002b3594e22684283d46934
MagicalBrain <13542593649@163.com>
2019-03-10 20:47:50 +0800

    本来不想写build函数的，看来不写会有点麻烦 下次再写吧

--- AVL-Tree/main.cpp
+++ AVL-Tree/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include "AvlTree.h"
 
 using namespace std;
 
+
+
 int main()
 {
+	int in[] = { 1,2,3,4,5,6,7,8,9,0 };
+	AvlTree T;
+	T.root = NULL;
+	
+	//AvlNode *t = T.root;
+
+	for (int i = 0; i < 10; i++)
+	{
+		insert(in[i], T);
+	}
+
+	int num = 1;
+	cout << "Find " << num << " is " << (find(num, T) ? "true" : "false") << endl;
+
 	return 0;
 }
